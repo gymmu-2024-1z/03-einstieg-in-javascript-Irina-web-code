4470e49093b98796c20a600d29efcff79576faad
Irina Giertz <e227295@sbl.ch>
2024-10-30 15:19:46 +0000

    Tag 2.3

--- script.js
+++ script.js
@@ -61,13 +61,16 @@ linkupExerciseHandler("[data-click=aufgabe02]", aufgabe02)
 export function aufgabe03(args) {
   const input = args
   const result = []
+  let count = 0
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     if (currentElement === "e") {
       count = count + 1
+    } else if (currentElement === "E") {
+      count++
     }
   }
-  return result.join("")
+  return count
 }
 linkupExerciseHandler("[data-click=aufgabe03]", aufgabe03)
